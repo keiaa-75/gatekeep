41fa42c2471c29e31fe0de6c6cc5a7a71d8f94c4
Keiaa <confinedrose@duck.com>
2025-11-16 20:30:28 +0800

    refactor(model): use lombok to reduce boilerplate in AttendanceRecord

--- src/main/java/com/shing/gatekeep/model/AttendanceRecord.java
+++ src/main/java/com/shing/gatekeep/model/AttendanceRecord.java
@@ -5,9 +5,16 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Transient;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.time.LocalDateTime;
 
 @Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class AttendanceRecord {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -21,28 +28,8 @@ public class AttendanceRecord {
     @Transient private Integer gradeLevel;
     @Transient private String section;
 
-    // Constructors
-    public AttendanceRecord() {}
     public AttendanceRecord(String lrn, LocalDateTime attendanceTime) {
         this.lrn = lrn;
         this.attendanceTime = attendanceTime;
     }
-
-    // Getters and Setters
-    public Long getId() { return id; }
-    public void setId(Long id) { this.id = id; }
-    public String getLrn() { return lrn; }
-    public void setLrn(String lrn) { this.lrn = lrn; }
-    public LocalDateTime getAttendanceTime() { return attendanceTime; }
-    public void setAttendanceTime(LocalDateTime attendanceTime) { this.attendanceTime = attendanceTime; }
-    public String getSurname() { return surname; }
-    public void setSurname(String surname) { this.surname = surname; }
-    public String getFirstName() { return firstName; }
-    public void setFirstName(String firstName) { this.firstName = firstName; }
-    public String getStrand() { return strand; }
-    public void setStrand(String strand) { this.strand = strand; }
-    public Integer getGradeLevel() { return gradeLevel; }
-    public void setGradeLevel(Integer gradeLevel) { this.gradeLevel = gradeLevel; }
-    public String getSection() { return section; }
-    public void setSection(String section) { this.section = section; }
 }
